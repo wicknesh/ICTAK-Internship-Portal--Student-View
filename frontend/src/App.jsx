6ceef19f35301b7eaa0b10519606e60cf00b90bc
enigmabloom <lakshminp671994@gmail.com>
2024-12-10 11:00:47 +0530

    Update App.jsx
    added project dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import HomePage from './components/Homepage'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 import CustomNavbar from './components/CustomNavbar';
 import Layout from './components/Layout';
+import ProjectBoard from './components/Project_board';
+import WeeklySubmission from './components/WeeklySubmission';
+import FinalSubmission from './components/FinalSubmission';
+import Vivavoce from './components/Vivavoce';
+import DiscussionForum from './components/DiscussionForum';
 
 
 
@@ -20,7 +26,15 @@ function App() {
 
   
 <Layout> <HomePage/></Layout>
-
+<BrowserRouter>
+      <Routes>
+       <Route path ="/" element={<ProjectBoard/>} />
+     <Route path="/weekly-submission" element={<WeeklySubmission />} />
+       <Route path="/final-submission" element={<FinalSubmission />} />  
+       <Route path='/vivavoce' element={<Vivavoce/>}/>
+       <Route path='/discussionforum' element={<DiscussionForum/>}/>
+      </Routes>
+     </BrowserRouter> 
 
     </>
   )
