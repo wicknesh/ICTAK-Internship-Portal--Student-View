daa2affc479e9de2e253c8cd172ae2d16213e1c8
Vignesh J S <js.vignesh98@gmail.com>
2024-12-16 00:26:17 +0530

    Remove unused import

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { TextField, Button, Typography } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
