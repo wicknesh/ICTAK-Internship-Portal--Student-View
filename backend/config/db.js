91c100cc10001095b99a45b50ad15b17e77af301
wicknesh <102829841+wicknesh@users.noreply.github.com>
2024-12-15 21:49:10 +0530

    Add MongoDB connection setup

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const options = {
+    dbName: 'internshipApp'
+};
+
+const connectDB = () => {
+    mongoose.connect(process.env.mongodb_url, options)
+        .then(() => console.log('DB is connected'))
+        .catch((error) => console.log(error));
+}
+
+export default connectDB;
