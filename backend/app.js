9ec6d0bc62528256ca1bb7cb0f129d563fe5ecc5
Vignesh J S <js.vignesh98@gmail.com>
2024-12-16 11:30:55 +0530

    Update origin link in cors options

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,9 @@ const studentRoutes =require("./routes/studentRoutes")
 
 var app=express()
 app.use(express.json());
+app.use(cors());
 app.use(cors({
-    origin: ["https://ictak-internship-portal-student-view.vercel.app/"],
+    origin: ["https://ictak-internship-portal-student-view-frontend.vercel.app/"],
     methods: ["POST", "PUT", "GET"],
     credentials: true
 }))
