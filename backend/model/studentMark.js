ad2b3e6f07375322c8387f2a4fb3756c5b09307b
Vignesh J S <js.vignesh98@gmail.com>
2024-12-16 16:40:44 +0530

    Delete connection.js studentData.js studentMark.js from the project to fix conflict

--- backend/model/studentMark.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const examSchema = new mongoose.Schema({
-    email: String,
-    mark: Number,
-});
-
-const examModel = mongoose.model('examscore', examSchema);
-module.exports = examModel;
