15c4a85d601446dd5ae11f714ccbfdb1a093f17c
Vignesh J S <js.vignesh98@gmail.com>
2024-12-16 10:58:35 +0530

    Add options to cors for deployement

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,11 @@ const studentRoutes =require("./routes/studentRoutes")
 
 var app=express()
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: ["https://ictak-internship-portal-student-view.vercel.app/"],
+    methods: ["POST", "PUT", "GET"],
+    credentials: true
+}))
 app.use('/student',studentRoutes)
 
 app.listen(process.env.port,()=>{
