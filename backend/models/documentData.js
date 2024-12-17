e9c2cd32191a2a7dfab09844e4c6fee045d2e250
Vignesh J S <js.vignesh98@gmail.com>
2024-12-17 17:59:05 +0530

    Add schema for document upload

---
+++ backend/models/documentData.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const documentSchema = new mongoose.Schema({
+    name: { type: String, required: true},
+    document: { type: Buffer, required: true },
+    project_id: { type: String, required: true }
+});
+
+export default mongoose.model('document', documentSchema);
